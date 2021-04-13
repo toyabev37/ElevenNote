f2a0485554cb67064c5f13b1f4c6f33e9b06bf5c
toyabev37 <toyabev37@gmail.com>
2021-04-13 15:21:17 -0400

    Add project files.

---
+++ ElevenNote.Data/Practice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    class Practice
+    {
+    }
+
+    public class Post
+    {
+        [Key]
+        public int int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Like> Likes { get; set; }
+        public Guid AuthorId { get; set; }
+
+    }
+
+    public class Comment
+
+        [Key]
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+        [ForeignKey] to Post via Id w/ virtual Post) 
+ 
+ 
+ 
+
+}
